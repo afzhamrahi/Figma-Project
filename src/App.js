f8e40e56d99e0097322349f4057485507cb86b93
Abolfazl Hamrahi <afzhmrahi@gmail.com>
2024-02-10 12:04:53 +0330

    master2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Home from './Components/Home/Home';
+import AboutMe from './Components/AboutMe/AboutMe';
+import Projects from './Components/Project-page/Projects';
+import Section from './Components/Section/Section';
+import Footer from './Components/Footer/Footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Home/>
+      <AboutMe/>
+      <Projects/>
+      <Section/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import Home from "./Components/Home/Home"
+
+
+ let roures =[
+    {path:"/" , Element:<Home/>},
+]
+
+
+export default roures
